58c66a5cebd52d95c37ac2a9804275d7224f065c
Deivison Cardoso <deivisondc@gmail.com>
2024-01-19 14:49:34 -0300

    [api] initial setup

---
+++ api/routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/contacts', [ContactController::class, 'index']);
+Route::post('/contacts', [ContactController::class, 'store']);
+Route::get('/contacts/{contact}', [ContactController::class, 'show']);
+Route::put('/contacts{contact}', [ContactController::class, 'update']);
+Route::delete('/contacts/{contact}', [ContactController::class, 'destroy']);
