def210ef9223319247d8de280cc039c6e1d9cba9
Deivison Cardoso <deivisondc@gmail.com>
2024-01-23 11:06:08 -0300

    [app] feat: add Files route

--- app/src/app/files/page.tsx
+++ app/src/app/files/page.tsx
@@ -1,9 +1,45 @@
+'use client'
+
+import { PageTitle } from "@/components/PageTitle";
+import { UploadButton } from "./components/UploadButton";
+import { TemplateFileButton } from "./components/TemplateFileButton";
 import { PageTitle } from "@/components/UI/PageTitle";
+import { useCallback, useEffect, useState } from "react";
+import { ResponseType } from "@/types/ResponseType";
+import { FileImportType } from "@/types/FileImportType";
 
 export default function Files() {
+  const [response, setResponse] = useState<ResponseType<FileImportType>>()
+
+  const fetchFiles = useCallback(async (page = 1) => {
+    const dataRaw = await fetch(`http://localhost:8000/api/files?page=${page}`);
+    const data = (await dataRaw.json()) as ResponseType<FileImportType>
+
+    setResponse(data)
+  }, [])
+
+  useEffect(() => {
+    fetchFiles()
+  }, [fetchFiles])
+
   return (
     <>
-      <PageTitle>Files</PageTitle>
+      <div className="flex justify-between items-start flex-row gap-2">
+        <PageTitle>Files</PageTitle>
+
+        <div className="flex flex-row items-center gap-4">
+          <TemplateFileButton />
+          <UploadButton onSuccess={fetchFiles} />
+        </div>
+      </div>
+
+      <ul>
+        {response && response.data.map(item => (
+          <li key={item.id}>
+            {item.filename}
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
