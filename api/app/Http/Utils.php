452bcce3b1cac26894b7fb536757de0b82fd22fd
Deivison Cardoso <deivisondc@gmail.com>
2024-01-23 12:03:55 -0300

    [api] feat: add utils to decrease the amount of links produced by paginate function

---
+++ api/app/Http/Utils.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http;
+
+class Utils
+{
+    public static function formatPaginationLinks($links, $lastPage, $currentPage)
+    {
+        $withoutPreviousAndNext = array_slice($links, 1, -1);
+        $emptyLink = [
+            "url" => null,
+            "label" => "...",
+            "active" => false
+        ];
+
+        $links = [];
+
+        if ($lastPage <= 5)  {
+            return $withoutPreviousAndNext;
+        } else {
+            if ($currentPage <= 3) {
+                for ($i=1; $i <= 3; $i++) {
+                    $links[] = [
+                        "label" => $i,
+                        "active"=> $currentPage == $i
+                    ];
+                }
+
+                if ($currentPage == 3) {
+                    $links[] = [
+                        "label" => 4,
+                        "active"=> $currentPage == 4
+                    ];
+                }
+
+                $links[] = $emptyLink;
+                $links[] = [
+                    "label" => $lastPage,
+                    "active"=> $currentPage == $lastPage
+                ];
+            } else if ($currentPage >= ($lastPage - 2)) {
+                $links[] = [
+                    "label" => 1,
+                    "active"=> $currentPage == 1
+                ];
+                $links[] = $emptyLink;
+
+                if ($currentPage == ($lastPage - 2)) {
+                    $links[] = [
+                        "label" => ($lastPage - 3),
+                        "active"=> $currentPage == ($lastPage - 3)
+                    ];
+                }
+
+                for ($i=($lastPage - 2); $i <= $lastPage; $i++) {
+                    $links[] = [
+                        "label" => $i,
+                        "active"=> $currentPage == $i
+                    ];
+                }
+            } else {
+                $links[] = [
+                    "label" => 1,
+                    "active"=> $currentPage == 1
+                ];
+                $links[] = $emptyLink;
+
+                for ($i=($currentPage - 1); $i <= $currentPage + 1; $i++) {
+                    $links[] = [
+                        "label" => $i,
+                        "active"=> $currentPage == $i
+                    ];
+                }
+
+                $links[] = $emptyLink;
+                $links[] = [
+                    "label" => $lastPage,
+                    "active"=> $currentPage == $lastPage
+                ];
+            }
+        }
+
+        return $links;
+    }
+}
